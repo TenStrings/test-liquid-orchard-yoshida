bf587a2021ee90fb91dd52787a8ee30dfde219fb
Enzo Cioppettini <noreply@enzocioppettini.com>
2020-12-01 01:20:15 -0300

    discard EMonad test

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,7 +18,6 @@ module Lib where
 import qualified Control.Concurrent.Chan as C
 import Control.Effect.Sessions
 import Data.Type.Set hiding ((:->))
-import EMonad
 import GHC.Exts (Constraint)
 import GHC.TypeLits
 import Unsafe.Coerce
@@ -29,10 +28,6 @@ mainFunc = run $
   do
     putStrLn "Running division magic service"
     divProc
-
--- mainFunc = getEMonad newTest'
--- mainFunc = getEMonad newTest
--- mainFunc = getEMonad newTest''
 
 data NonZero = NZ Int deriving (Show)
 
@@ -55,7 +50,7 @@ divClient ::
 divClient (c :: Chan (Op "c")) (d :: (Chan (Op "d"))) =
   do
     send c (2 :: Int)
-    send c (NZ 1)
+    send c (NZ 0)
     answer <- (recv d :: Process _ Int)
     putStrLn $ "result " ++ show answer
 
@@ -66,13 +61,5 @@ divProc = new $ \(c, c') -> new $ \(d, d') -> divServer c d `par` divClient c' d
 -- nonzero = NZ 0
 
 -- this doesn't
-nonzero :: NonZero
-nonzero = NZ 1
-
-newTest'' = eclean $ do
-  chan <- eliftIO C.newChan
-  chan <- eclean $ return (EMkChan chan :: EChan "chan")
-  esend chan $ NZ 1
-  (NZ t) <- erecv chan
-  eliftIO $ P.putStrLn $ show t
-  return ()
+-- nonzero :: NonZero
+-- nonzero = NZ 1
